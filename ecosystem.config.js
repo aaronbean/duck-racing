c92510a48dc16ebaaab79c35244c98243c8b4573
aaronbean <aaron.bean@beardon.com>
2024-10-11 11:47:53 -0500

    wrong PM2 path name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
             name: 'duck-racing',
             script: 'serve',
             env: {
-                PM2_SERVE_PATH: './duck-racing/dist',
+                PM2_SERVE_PATH: './app/dist',
                 PM2_SERVE_PORT: 5173,
                 PM2_SERVE_SPA: 'true',
             },
