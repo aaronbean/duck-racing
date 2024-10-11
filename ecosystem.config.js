1a16a6622791e087afac505bc5186df3ee9bddb8
aaronbean <aaron.bean@beardon.com>
2024-10-11 11:43:54 -0500

    breaking ecosystem file outside of app to avoid ESM issues

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,12 @@
 module.exports = {
     apps: [
         {
-            name: `duck-racing`,
-            script: "--host",
+            name: 'duck-racing',
+            script: 'serve',
             env: {
-                PM2_SERVE_PATH: "./dist",
+                PM2_SERVE_PATH: './duck-racing/dist',
                 PM2_SERVE_PORT: 5173,
-                PM2_SERVE_SPA: "true",
-                NODE_ENV: 'development',
+                PM2_SERVE_SPA: 'true',
             },
         },
     ],
